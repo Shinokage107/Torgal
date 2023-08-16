3e086897a0308e8b1d5ccb7c570254d4861eaf5c
Alexander Braun Home <alexanderbraun121@gmail.com>
2023-08-16 14:27:06 +0200

    ffxiv profile command has now an option for patch completion and to mention a different user. added more pet gifs

--- commands/user/ffxiv_show.js
+++ commands/user/ffxiv_show.js
@@ -1,26 +1,81 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { ffxivCollect } = require("../../api/api.js");
+const path = require("path");
 module.exports = {
   type: "user",
-  data: new SlashCommandBuilder().setName("ffxiv").setDescription("Shows completion stats of your FFXIV character."),
+  data: new SlashCommandBuilder()
+    .setName("ffxiv")
+    .setDescription("Shows completion stats of your FFXIV character.")
+    .addUserOption((option) => option.setName("user").setDescription("@Discord User"))
+    .addIntegerOption((option) =>
+      option
+        .setName("patch")
+        .setDescription("Selected Patch")
+        .addChoices(
+          { name: "Endwalker", value: 6 },
+          { name: "Shadowbringer", value: 5 },
+          { name: "Stormblood", value: 4 },
+          { name: "Heavensward", value: 3 },
+          { name: "A Realm Reborn", value: 2 }
+        )
+    ),
 
   async execute(interaction) {
     await interaction.deferReply();
+
+    var userId = interaction.options.getUser("user") != null ? interaction.options.getUser("user") : interaction.user.id;
+
     await ffxivCollect
-      .get("users/" + interaction.user.id.toString())
-      .then((response) => {
+      .get("users/" + userId)
+      .then(async (response) => {
         var characterData;
         characterData = response;
+        const decimalCount = 2;
 
-        var mountPercentage = ((characterData.mounts.count / characterData.mounts.total) * 100).toFixed(2);
-        var minionPercentage = ((characterData.minions.count / characterData.minions.total) * 100).toFixed(2);
-        var achievementPercentage = ((characterData.achievements.count / characterData.achievements.total) * 100).toFixed(2);
+        var mountPercentage = ((characterData.mounts.count / characterData.mounts.total) * 100).toFixed(decimalCount);
+        var minionPercentage = ((characterData.minions.count / characterData.minions.total) * 100).toFixed(decimalCount);
+        var achievementPercentage = ((characterData.achievements.count / characterData.achievements.total) * 100).toFixed(decimalCount);
+        var bluePercentage = ((characterData.spells.count / characterData.spells.total) * 100).toFixed(decimalCount);
+        var relicsPercentage = (
+          ((characterData.relics.weapons.count + characterData.relics.tools.count + characterData.relics.armor.count) /
+            (characterData.relics.weapons.total + characterData.relics.tools.total + characterData.relics.armor.total)) *
+          100
+        ).toFixed(decimalCount);
+        var orchestPersentage = ((characterData.orchestrions.count / characterData.orchestrions.total) * 100).toFixed(decimalCount);
+
+        /// Completion calculations and filtering
+
+        if (interaction.options.getInteger("patch") != null) {
+          patch = interaction.options.getInteger("patch");
+
+          mountPercentage = await fillterPercentage(userId, "mounts", patch, decimalCount);
+          achievementPercentage = await fillterPercentage(userId, "achievements", patch, decimalCount);
+          minionPercentage = await fillterPercentage(userId, "minions", patch, decimalCount);
+          bluePercentage = await fillterPercentage(userId, "spells", patch, decimalCount);
+          relicsPercentage = await fillterPercentage(userId, "relics", patch, decimalCount);
+        }
+
+        var completionPercentage = (
+          (parseFloat(mountPercentage) +
+            parseFloat(minionPercentage) +
+            parseFloat(achievementPercentage) +
+            parseFloat(bluePercentage) +
+            parseFloat(relicsPercentage)) /
+          5
+        ).toFixed(decimalCount);
+
         var verified = characterData.verified ? " <:tickyes:1139024765621903452>" : " <:tickno:1139024530808000582>";
+        var footer = characterData.verified ? " " : " This Character is not Verified!";
+
+        var completionType =
+          interaction.options.getInteger("patch") != null
+            ? "Patch Completion **" + interaction.options.getInteger("patch") + ".0** : "
+            : "Overall Completion: ";
 
         const showCaseEmbed = new EmbedBuilder()
           .setColor(0x0099ff)
           .setTitle(characterData.name + "@" + characterData.server)
-          .setDescription("Verified: " + verified)
+          .setDescription("<@" + userId + "> " + verified + "\n" + completionType + completionPercentage + " %")
           .setThumbnail(characterData.avatar)
           .addFields({
             name: "Mounts",
@@ -37,15 +92,66 @@ module.exports = {
             value: "<:sorcerer:839189024585154580> " + achievementPercentage + " %",
             inline: true,
           })
+          .addFields({
+            name: "Blue",
+            value: "<:coven:839189024518176779> " + bluePercentage + " %",
+            inline: true,
+          })
+          .addFields({
+            name: "Relics",
+            value: "<:redeemed:839189024610320384> " + relicsPercentage + " %",
+            inline: true,
+          })
+          // .addFields({
+          //   name: "Orchestrions",
+          //   value: "<:invoker:839189024529973259> " + orchestPersentage + " %",
+          //   inline: true,
+          // })
           .setFooter({
-            text: characterData.last_parsed,
+            text: characterData.last_parsed + footer,
           });
 
         interaction.followUp({ embeds: [showCaseEmbed] });
       })
       .catch((exception) => {
+        console.log(exception);
         if (exception.response.data.status == 404) interaction.followUp(exception.response.data.error);
         else interaction.followUp("Something went wrong <:tickno:1139024530808000582>");
       });
   },
 };
+
+async function fillterPercentage(userId, filter, condition, decimalCount) {
+  var owned;
+  var missing;
+  await ffxivCollect.get("users/" + userId + "/" + filter + "/owned").then((response) => {
+    owned = response;
+  });
+
+  await ffxivCollect.get("users/" + userId + "/" + filter + "/missing").then((response) => {
+    missing = response;
+  });
+
+  var ownedFiltered = 0;
+  var missingFiltered = 0;
+
+  if (filter != "relics") {
+    owned.forEach((element) => {
+      if (Math.floor(element.patch) == condition) ownedFiltered++;
+    });
+    missing.forEach((element) => {
+      if (Math.floor(element.patch) == condition) missingFiltered++;
+    });
+  } else if (filter == "relics") {
+    owned.forEach((element) => {
+      if (Math.floor(element.type.expansion) == condition) ownedFiltered++;
+    });
+    missing.forEach((element) => {
+      if (Math.floor(element.type.expansion) == condition) missingFiltered++;
+    });
+  }
+
+  var output = ((ownedFiltered / (ownedFiltered + missingFiltered)) * 100).toFixed(decimalCount);
+
+  return isNaN(output) ? 100.0 : output;
+}
--- commands/user/pet.js
+++ commands/user/pet.js
@@ -5,8 +5,18 @@ module.exports = {
 
   async execute(interaction) {
     await interaction.deferReply();
-    await interaction.followUp(
-      "https://images-ext-2.discordapp.net/external/SbMMg5khyMeaKv2xK57qtV8BOXnZnMgeJCynQqpaqTs/https/media.tenor.com/244r7kU5Y_YAAAAC/torgal-clive-rosfield.gif"
-    );
+    var links = [
+      "https://images-ext-2.discordapp.net/external/SbMMg5khyMeaKv2xK57qtV8BOXnZnMgeJCynQqpaqTs/https/media.tenor.com/244r7kU5Y_YAAAAC/torgal-clive-rosfield.gif",
+      "https://media.tenor.com/4HQ6vlnN32MAAAAC/torgal-torgal-puppy.gif",
+      "https://media.tenor.com/v_i2K_i_EVYAAAAC/torgal.gif",
+      "https://64.media.tumblr.com/1a3dbb9815823e99f15594fba75c5e78/f0436a6f64389d9a-1e/s1280x1920/4f6cc810c157f5feac945ea9f4813d091e036d50.gif",
+      "https://64.media.tumblr.com/569d2cd450cffe73f13de5987a329678/86454588dc55e6c4-07/s540x810/0f929df6e7c2511b6f4d9276e98b110071cd1074.gif",
+    ];
+    var number = getRandomInt(links.length);
+    await interaction.followUp(links[number]);
   },
 };
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
