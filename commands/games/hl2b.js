bea19c2488b2802f1b007e3e724d1e132f85aabe
Alexander Braun Home <alexanderbraun121@gmail.com>
2024-01-13 12:42:57 +0100

    Implementation of a How Long 2 beat command.

---
+++ commands/games/hl2b.js
@@ -0,0 +1,65 @@
+const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder } = require("discord.js");
+const { Pagination } = require("@acegoal07/discordjs-pagination");
+const { hl2b } = require("../../src/api.js");
+module.exports = {
+  type: "dev",
+  data: new SlashCommandBuilder()
+    .setName("hl2b")
+    .setDescription("Ping pong command but only for dev")
+    .addStringOption((option) => option.setName("name").setDescription("The Name of the Game").setRequired(true)),
+  execute: execute,
+};
+
+async function execute(interaction) {
+  await interaction.deferReply();
+  var pages = 20;
+  var book = [];
+
+  input = interaction.options.getString("name");
+  await hl2b
+    .get("?name=" + input)
+    .then((response) => {
+      for (let index = 0; index < pages && index < response.length; index++) {
+        const page = response[index];
+
+        console.log(page, page.imageUrl, page.name);
+
+        var print = new EmbedBuilder();
+
+        print.setTitle(page.name);
+        print.setDescription("Platforms: " + page.platforms);
+        print.setThumbnail(page.imageUrl).addFields({
+          name: "----------",
+          value:
+            "<:PlayStation_bronze_trophy:1195693685984211075> **Main:**  " +
+            page.gameplayMain +
+            "h\n<:PlayStation_Silver_Trophy:1195693645517570130> **Main + Extra:**  " +
+            page.gameplayMainExtra +
+            "h\n<:PlayStation_Gold_Trophy:1195693593894080625> **Completionist:**  " +
+            page.gameplayCompletionist +
+            "h",
+          inline: false,
+        });
+        book.push(print);
+      }
+
+      if (book.length > 0) {
+        new Pagination()
+          .setPortal(interaction)
+          .setPageList(book)
+          .enableAutoButton()
+          .setProgressBar()
+          .enableAuthorIndependent()
+          .disableDisabledButtons()
+          .setTimeout(30000)
+          // .enablePrivateReply()
+          .paginate();
+      } else {
+        interaction.followUp("Sry i was not able to find anything :c");
+      }
+    })
+    .catch((exception) => {
+      console.log(exception);
+      interaction.followUp("Something went wrong <:tickno:1139024530808000582>");
+    });
+}
--- src/api.js
+++ src/api.js
@@ -4,9 +4,12 @@ const ffxivCollect = new Api("https://ffxivcollect.com/api", [new DefaultRespons
 
 const lodestonenews = new Api("https://na.lodestonenews.com", [new DefaultResponseProcessor(DefaultApiException), onlyDataProcessor]);
 
+const hl2b = new Api("https://hltb-api.vercel.app/api", [new DefaultResponseProcessor(DefaultApiException), onlyDataProcessor]);
+
 function onlyDataProcessor(response) {
   return Promise.resolve(response.data);
 }
 
 module.exports.ffxivCollect = ffxivCollect;
 module.exports.lodestonenews = lodestonenews;
+module.exports.hl2b = hl2b;
